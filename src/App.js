6d81d6e1681446dd59aface0ecd3aa4315e13e14
naiyoma <lankas.aurelia@gmail.com>
2022-03-10 03:30:17 +0300

    add meme components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import React from "react"
 import Header from './components/Header';
+import Meme from './components/Meme';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Meme />
     </div>
   );
 }
